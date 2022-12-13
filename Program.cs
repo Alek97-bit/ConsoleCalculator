f5494a1da5d9ff2ea19ebf5ddd0e3539c6bda4c3
Oleksii <118004578+Alek97-bit@users.noreply.github.com>
2022-12-13 13:48:03 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,30 +10,36 @@ namespace Calculator
         public static void Main()
         {
             double number1 = 0;
-            double number2 = 0;
-          
+
+
             Console.WriteLine("Wpisz numer.");
             number1 = Convert.ToDouble(Console.ReadLine());
-            for (int i = 0; i < 10; i++)
-            {
-                string symbol;
-                Console.WriteLine("Wpisz znak");
-                symbol = Console.ReadLine();
+            Cal(number1);
 
-                if (symbol == "V")
+        }
+        static void Cal(double number1)
+        {
+            if (symbol == "V" || symbol == "!")
+            {
+                for (int i = 0; i < 10; i++)
                 {
-                    number1 = Math.Sqrt(number1);
-                    Console.WriteLine(Math.Round(number1,2));
+                    double number2 = 0;
+                    string symbol;
                     Console.WriteLine("Wpisz znak");
                     symbol = Console.ReadLine();
-                }
-                else
-                  if (symbol == "!")
-                {
-                    // int number1 = (int)(number1);
-                    for (double t = number1 - 1; t > 0; t--)
+
+                    if (symbol == "V")
                     {
-                        number1 = number1 * t;
+                        number1 = Math.Sqrt(number1);
+                    }
+                    else
+                      if (symbol == "!")
+                    {
+                        // int number1 = (int)(number1);
+                        for (double t = number1 - 1; t > 0; t--)
+                        {
+                            number1 = number1 * t;
+                        }
                     }
                     Console.WriteLine(Math.Round(number1, 2));
                     Console.WriteLine("Wpisz znak");
@@ -79,13 +85,13 @@ namespace Calculator
                     number1 = (number1 / number2) * 100;
                 }
                 else
-   
+
                 if (symbol == "C")
                 {
                     Main();
                 }
 
-                Console.WriteLine(Math.Round(number1,2));
+                Console.WriteLine(Math.Round(number1, 2));
 
             }
         }
